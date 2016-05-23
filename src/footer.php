d4a58e0206d57dd4d51072e967649c742353186a
unknown <mikkel.lundgreen@BS024956.bestcorp.net>
2016-05-23 20:03:40 +0200

    Added underscores

---
+++ src/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ted_x
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ted_x' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ted_x' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ted_x' ), 'ted_x', '<a href="http://underscores.me/" rel="designer">TEDx Aarhus</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
