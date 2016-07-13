7eb6af018a16f5257be46d257a68dc0379cb0201
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-13 17:41:52 +0200

    Add markup and placeholders for the footer

--- src/footer.php
+++ src/footer.php
@@ -14,10 +14,27 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<p class="copyright-notice"><?php printf( esc_html( 'This independent TEDx event is operated under license from %s.', 'ted_x' ), '<a href="https://www.ted.com/">TED</a>' ); ?></p>
-			<p class="copyright-notice"><?php printf( esc_html( 'Copyright &copy; %s TEDxAarhus', 'ted_x' ), date( 'Y' ) ); ?></p>
-		</div><!-- .site-info -->
+		<section id="main-footer">
+			<div class="container">
+				<div class="newsletter">
+
+				</div><!-- .newsletter -->
+				<div class="navigation">
+
+				</div><!-- .navigation -->
+				<div class="social-media">
+
+				</div><!-- .social-media -->
+			</div><!-- .container -->
+		</section><!-- #main-footer -->
+		<section id="sub-footer">
+			<div class="container">
+				<p class="copyright-notice">
+					<?php printf( esc_html( 'This independent TEDx event is operated under license from %s.', 'ted_x' ), '<a href="https://www.ted.com/">TED</a>' ); ?><br>
+					<?php printf( esc_html( 'Copyright &copy; %s TEDxAarhus', 'ted_x' ), date( 'Y' ) ); ?>
+				</p>
+			</div><!-- .container -->
+		</section><!-- #sub-footer -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
