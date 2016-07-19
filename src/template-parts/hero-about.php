2295fc688b0a162da10418b010140878bdb48253
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-19 16:15:41 +0200

    Add hero unit markup and hardcoded content to the About Us page

---
+++ src/template-parts/hero-about.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template part for displaying the hero unit on the About Us page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ted_x
+ */
+
+?>
+
+<section class="hero-unit full-width about-us">
+
+	<div class="center">
+
+		<div class="quote">
+			<p class="right">The second year of TEDx in Aarhus,<br> the first TEDxAarhus ever!</p>
+		</div><!-- .quote -->
+
+		<div class="infobox">
+			<p>In 2016 we are delighted to present TEDx for the second year in Aarhus, a vibrant city in Denmark where many great ideas are born. We finally got the extended license, allowing us to have more than 100 attendees and X’ify the name of our beloved city – Aarhus!</p>
+		</div><!-- .infobox -->
+
+	</div><!-- .center -->
+
+</section><!-- .hero-unit -->
