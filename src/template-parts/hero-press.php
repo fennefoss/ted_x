ee5856dce7833f74f6486de7549c37e7e43cc79b
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-21 18:07:36 +0200

    Add a Custom Press Page with Hero Unit based on the About Us page

---
+++ src/template-parts/hero-press.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying the hero unit on the Press page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ted_x
+ */
+
+?>
+
+<section class="hero-unit full-width press">
+
+	<div class="center">
+
+		<div class="infobox">
+			<p>Press and media coverage of the events and about the TEDxAarhus are always welcome.</p>
+			<p>&nbsp;</p>
+			<p>All press requests for the official TEDxAarhus logo, photographs, press-releases, write-ups or any press-related information should be emailed to the marketing group: <a href="mailto:marketing@tedxaarhus.com">marketing@tedxaarhus.com</a>.</p>
+		</div><!-- .infobox -->
+
+	</div><!-- .center -->
+
+</section><!-- .hero-unit -->
