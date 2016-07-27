be547436d421fe9299dd84d3c8003bf525c48101
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-27 14:10:13 +0200

    Remove widget title

--- src/inc/widgets.php
+++ src/inc/widgets.php
@@ -31,8 +31,6 @@ class SoMe_Widget extends WP_Widget {
 	 * @param array $instance Saved values from database.
 	 */
 	public function widget( $args, $instance ) {
-		// Determine if a title has been set or default to 'Social Media Icons' and apply the `widget_title` filter
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Social Media Icons', 'ted_x' ) : $instance['title'], $instance, $this->id_base );
 		// Check for social media URLs set in the database
 		$url_facebook  = isset( $instance['url_facebook'] ) ? $instance['url_facebook'] : false;
 		$url_twitter   = isset( $instance['url_twitter'] ) ? $instance['url_twitter'] : false;
@@ -55,7 +53,6 @@ class SoMe_Widget extends WP_Widget {
 
 		// Output all the markup to STDOUT as per WordPress protocol
 		echo $args['before_widget'];
-		echo $args['before_title'] . $title . $args['after_title'];
 		echo '<aside class="social-media-icons">' . $markup . '</aside>';
 		echo $args['after_widget'];
 	}
@@ -69,14 +66,11 @@ class SoMe_Widget extends WP_Widget {
 	 */
 	public function form( $instance ) {
 		$instance = wp_parse_args( (array) $instance, array(
-			'title'         => '',
 			'url_facebook'  => '',
 			'url_twitter'   => '',
 			'url_youtube'   => '',
 			'url_snapchat'  => '',
 		) );
-		// Fetch and sanitize the title field
-		$title = sanitize_text_field( $instance['title'] );
 		// Fetch and sanitize social media profile URLs
 		// using `wp_strip_all_tags()` to avoid breaking URLs
 		$url_facebook  = wp_strip_all_tags( $instance['url_facebook'] );
@@ -86,23 +80,22 @@ class SoMe_Widget extends WP_Widget {
 		// Dump the markup for the setup form straight to STDOUT
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+			Enter the URLs for the social network profiles that the widget icons should link to. Icons will only be shown if a URL has been entered in the corresponding field in the form below.
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'url_facebook' ); ?>"><?php _e( 'Facebook URL:' ); ?></label>
+			<label for="<?php echo $this->get_field_name( 'url_facebook' ); ?>"><?php _e( 'Facebook:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'url_facebook' ); ?>" name="<?php echo $this->get_field_name( 'url_facebook' ); ?>" type="url" value="<?php echo esc_attr( $url_facebook ); ?>">
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'url_twitter' ); ?>"><?php _e( 'Twitter URL:' ); ?></label>
+			<label for="<?php echo $this->get_field_name( 'url_twitter' ); ?>"><?php _e( 'Twitter:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'url_twitter' ); ?>" name="<?php echo $this->get_field_name( 'url_twitter' ); ?>" type="url" value="<?php echo esc_attr( $url_twitter ); ?>">
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'url_youtube' ); ?>"><?php _e( 'YouTube Channel URL:' ); ?></label>
+			<label for="<?php echo $this->get_field_name( 'url_youtube' ); ?>"><?php _e( 'YouTube:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'url_youtube' ); ?>" name="<?php echo $this->get_field_name( 'url_youtube' ); ?>" type="url" value="<?php echo esc_attr( $url_youtube ); ?>">
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'url_snapchat' ); ?>"><?php _e( 'Snapchat URL:' ); ?></label>
+			<label for="<?php echo $this->get_field_name( 'url_snapchat' ); ?>"><?php _e( 'Snapchat:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'url_snapchat' ); ?>" name="<?php echo $this->get_field_name( 'url_snapchat' ); ?>" type="url" value="<?php echo esc_attr( $url_snapchat ); ?>">
 		</p>
 		<?php
@@ -121,15 +114,12 @@ class SoMe_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		// Set defaults for $instance and $new_instance to avoid `undefined index` notices
 		$instance = [
-			'title'         => '',
 			'url_facebook'  => '',
 			'url_twitter'   => '',
 			'url_youtube'   => '',
 			'url_snapchat'  => '',
 		];
 		$new_instance = wp_parse_args( $new_instance, $instance );
-		// Populate `$instance` with filtered values from widget options page
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
 		// if we use sanitize_text_field() here we might break the URLs
 		$instance['url_facebook']  = wp_strip_all_tags( $new_instance['url_facebook'] );
 		$instance['url_twitter']   = wp_strip_all_tags( $new_instance['url_twitter'] );
