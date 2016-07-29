66e2267385a1c6f73bc25e0826390fa3152b51d7
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-29 14:46:01 +0200

    Implement Advanced Custom Fields on About Us and Press pages

--- src/template-parts/hero-about.php
+++ src/template-parts/hero-about.php
@@ -13,13 +13,17 @@
 
 	<div class="center">
 
+	<?php if ( get_field( 'about_hero_quote' ) ) : ?>
 		<div class="quote">
-			<p class="right">The second year of TEDx in Aarhus,<br> the first TEDxAarhus ever!</p>
+			<p class="right"><?php the_field( 'about_hero_quote' ); ?></p>
 		</div><!-- .quote -->
+	<?php endif; ?>
 
+	<?php if ( get_field( 'about_hero_infobox' ) ) : ?>
 		<div class="infobox">
-			<p>In 2016 we are delighted to present TEDx for the second year in Aarhus, a vibrant city in Denmark where many great ideas are born. We finally got the extended license, allowing us to have more than 100 attendees and X’ify the name of our beloved city – Aarhus!</p>
+			<?php the_field( 'about_hero_infobox' ); ?>
 		</div><!-- .infobox -->
+	<?php endif; ?>
 
 	</div><!-- .center -->
 
