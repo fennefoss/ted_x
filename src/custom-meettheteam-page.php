4184fa888d5a4ce54535208c89a5b15188a7575e
Tobias Møller Kjærsgaard <tmkjarsgaard@tdm.au.dk>
2016-07-29 14:24:51 +0200

    Trim unused markup and custom fields from the Meet The Team page

--- src/custom-meettheteam-page.php
+++ src/custom-meettheteam-page.php
@@ -14,72 +14,22 @@
 
 get_header(); ?>
 
-	<?php
-		$meettheteam_intro_headline = get_field('meettheteam_intro_headline');
-		$meettheteam_intro_p = get_field('meettheteam_intro_p');
-
-		
-	?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<section class="red-page-header">
-			<div class="red-page-header__line"></div>
-			<h2 class="red-page-header__headline"><?php echo $meettheteam_intro_headline; ?></h2>
-			<p class="red-page-header__text"><?php echo $meettheteam_intro_p; ?></p>
-		</section>
 
-
-
-
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			<section class="red-page-header">
+				<h2><?php the_field('meettheteam_intro_headline'); ?></h2>
+				<p><?php the_field('meettheteam_intro_p'); ?></p>
+			</section>
 
 			<?php
-			endif;
-
-			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				get_template_part( 'template-parts/content', 'page' );
 
-			endwhile;
+			endwhile; // End of the loop.
+			?>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-		<section class="meettheteam_container">
-			<div class="locotweet-line one"></div>
-			<h2 class="meettheteam_company_headline"><?php echo $meettheteam_company_headline; ?></h2>
-			<p class="meettheteam_company_p"><?php echo $meettheteam_company_p; ?></p>
-			
-			<div class="locotweet-line two"></div>
-			<h2 class="meettheteam_benefits_headline"><?php echo $meettheteam_benefits_headline; ?></h2>
-			<p class="meettheteam_benefits_p"><?php echo $meettheteam_benefits_p; ?></p>
-			
-			<div class="locotweet-line three"></div>
-			<h2 class="meettheteam_shake_headline"><?php echo $meettheteam_shake_headline; ?></h2>
-			<p class="meettheteam_shake_p"><?php echo $meettheteam_shake_p; ?></p>
-			
-			<div class="locotweet-line four"></div>
-			<h2 class="meettheteam_TEDxAarhusLive2016_headline"><?php echo $meettheteam_TEDxAarhusLive2016_headline; ?></h2>
-			<p class="meettheteam_TEDxAarhusLive2016_p"><?php echo $meettheteam_TEDxAarhusLive2016_p; ?></p>
-		</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
