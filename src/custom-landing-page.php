db16bc1cf6e945b4aa349414d8abf4d09afdb2ef
Tobias Møller Kjærsgaard <tobias@dijon.dk>
2016-08-06 11:57:29 +0200

    Remove news section from front page

--- src/custom-landing-page.php
+++ src/custom-landing-page.php
@@ -72,50 +72,6 @@ get_header(); ?>
 			<aside class="locotweet-x"></aside>
 		</section>
 
-		<section class="latest-news">
-			<h2 class="show-all-news__header"><?php echo $show_all_news_header; ?></h2>
-			<!--Super awesome snippet for showing the
-			posts from a certain category ID. Replace the
-			cat=ID with the number of the category wanted -->
-			<?php query_posts('category_name=News'); ?>
-		<section>
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-
-			<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		<button class="show-all-news">
-			<a class="show-all-news__link" href="<?php echo $show_all_news_link; ?>"><?php echo $show_all_news_link_text; ?></a>
-		</button>
-
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
